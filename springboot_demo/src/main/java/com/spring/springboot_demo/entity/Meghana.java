88fc849cd608d6e0e918788332f37203032244b1
Manimeghana2200 <manipodili00@gmail.com>
2024-11-09 13:38:33 +0530

    added meghana entity and modified employee entity

--- springboot_demo/src/main/java/com/spring/springboot_demo/entity/Meghana.java
+++ springboot_demo/src/main/java/com/spring/springboot_demo/entity/Meghana.java
@@ -1,9 +1,11 @@
 package com.spring.springboot_demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Meghana {
-	
+	@Id
 	private String name;
 	private int phoneNo;
-	
-
 }
